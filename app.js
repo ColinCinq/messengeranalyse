1933412d4524f608509018d36963f40ba268be46
Colin <colin.cinq@gmail.com>
2020-11-06 14:12:49 +0100

    add config de base serveur node

---
+++ app.js
@@ -0,0 +1,7 @@
+var server = require('./lib/server'),
+    handler = require('./lib/handler')
+
+require('./lib/router')(server, handler)
+
+console.log( server.settings.port )
+server.listen( server.settings.port )
