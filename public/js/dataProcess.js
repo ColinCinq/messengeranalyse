2601fc1de832775b6a892a11aafd55538280d650
Colin <colin.cinq@gmail.com>
2020-11-24 02:40:04 +0100

    WIP separation for processing and treatment

--- public/js/dataProcess.js
+++ public/js/dataProcess.js
@@ -23,149 +23,54 @@ dataProcess = {
      * @param participantList
      */
     getTotalMsg : function(format, participantList){
-        let data = [], ret = {}
-
-        /**
-         * create an array whit all formatted date as string
-         * @param dateFormat
-         * @param addMethod
-         */
-        function iterableDate(dateFormat, addMethod) {
-            let ret = []
-            for (let i = new Date(startDate) ; i.format(dateFormat)<=endDate.format(dateFormat) ; i[addMethod](1)){
-                ret.push(i.format(dateFormat))
+        let process = [
+            (ret, data) => {
+                for (m of messagesList) {
+                    this.fillRet(m, ret, data, function(message, parent, data) {
+                        if (!isNaN(parent[data[0].property(message)]))
+                            parent[data[0].property(message)] ++
+                    })
+                }
             }
-            return ret
-        }
-        /**
-         * create the object that contain the data
-         * @param parent
-         * @param data =>array(
-         *  {
-         *      iterable : array of key,
-         *      value : the value of the key (obj or number)
-         *  },
-         *  [...]
-         *)
-         */
-        function createRet(parent, data) {
-            for (key of data[0].iterable){
-                parent[key] = data[1] ? {} : 0
-                if (data[1])
-                    createRet(parent[key], data.slice(1))
-            }
-        }
-        /**
-         * fill the object that contain the data
-         * @param message
-         * @param parent
-         * @param data =>array(
-         *  {
-         *      iterable : array of key,
-         *      value : the value of the key (obj or number)
-         *  },
-         *  [...]
-         *)
-         */
-        function fillRet(message, parent, data) {
-            if (data[1]){
-                fillRet(message, parent[data[0].property(message)], data.slice(1))
-            }else{
-                if (!isNaN(parent[data[0].property(message)]))
-                    parent[data[0].property(message)] ++
-            }
-        }
-
-        switch (format) {
-            case 'hour':
-                data.push(
-                    {
-                        iterable : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-                        property : (m) => {
-                            return new Date(m.timestamp_ms).format('h')
-                        }
-                    }
-                )
-                break
-            case 'week':
-                data.push(
-                    {
-                        iterable : ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'],
-                        property : (m) => {
-                            return new Date(m.timestamp_ms).format('DD')
-                        }
-                    }
-                )
-                break
-            case 'weekhour':
-                data.push(
-                    {
-                        iterable : ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'],
-                        property : (m) => {
-                            return new Date(m.timestamp_ms).format('DD')
-                        }
-                    },
-                    {
-                        iterable : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-                        property : (m) => {
-                            return new Date(m.timestamp_ms).format('h')
-                        }
-                    }
-                )
-                break
-            case 'day':
-                data.push(
-                    {
-                        iterable : iterableDate('yyyy-MM-dd','addDays'),
-                        property : (m) => {
-                            return new Date(m.timestamp_ms).format('yyyy-MM-dd')
-                        }
-                    }
-                )
-                break
-            case 'month':
-                data.push(
-                    {
-                        iterable : iterableDate('yyyy-MM','addMonths'),
-                        property : (m) => {
-                            return new Date(m.timestamp_ms).format('yyyy-MM')
-                        }
-                    }
-                )
-                break
-            case 'year':
-                data.push(
-                    {
-                        iterable : iterableDate('yyyy','addYears'),
-                        property : (m) => {
-                            return new Date(m.timestamp_ms).format('yyyy')
-                        }
-                    }
-                )
-                break
-            default:
-                return messagesList.length
-        }
-
-        if (participantList){
-            data.push(
-                {
-                    iterable : participantList,
-                    property : (m) => {
-                        return m.sender_name
+        ]
+        return this.processing(process, format, participantList)
+    },
+    /**
+     * fill the object that contain the data
+     * @param format => hour, day, week, month, year, weekhour
+     * @param participantList
+     */
+    getResponseTime : function(format, participantList){
+        let process = [
+            (ret, data) => {
+                for (m in messagesList) {
+                    let currentMsg = messagesList[m],
+                        lastMsg = messagesList[m > 0 ? m-1 : 0]
+                    if (currentMsg.sender_name !== lastMsg.sender_name) {
+                        this.fillRet(currentMsg, ret, data, (message, parent, data) => {
+                            if (parent[data[0].property(message)] !== undefined) {
+                                if (parent[data[0].property(message)] === 0)
+                                    parent[data[0].property(message)] = []
+                                parent[data[0].property(message)].push(currentMsg.timestamp_ms - lastMsg.timestamp_ms)
+                            }
+                        })
                     }
                 }
-            )
-        }
-
-        createRet(ret, data)
-        for (m of messagesList) {
-            fillRet(m, ret, data)
-        }
-        return ret
+            },
+            (ret, data) => {
+                this.fillRet({}, ret, data, (message, parent, data) => {
+                    if (parent[data[0].property(message)] !== undefined) {
+                        if (parent[data[0].property(message)] === 0)
+                            parent[data[0].property(message)] = []
+                        parent[data[0].property(message)].push(currentMsg.timestamp_ms - lastMsg.timestamp_ms)
+                    }
+                })
+            }
+        ]
+        return this.processing(process, format, participantList)
     },
     // arg : nothing (all time) day/month/year (group by)
-    getResponseTime : function(format){
+    getResponseTime2 : function(format){
         let dateFormat, addMethod, skip=false, ret = {}
         switch (format) {
             case 'day':
@@ -279,6 +184,152 @@ dataProcess = {
         }
 
         return ret
+    },
+
+    /**
+     * fill the object that contain the data
+     * @param process
+     * @param format => hour, day, week, month, year, weekhour
+     * @param participantList
+     */
+    processing : function(process, format, participantList){
+        let data = [], ret = {}
+
+        switch (format) {
+            case 'hour':
+                data.push(
+                    {
+                        iterable : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+                        property : (m) => {
+                            return new Date(m.timestamp_ms).format('h')
+                        }
+                    }
+                )
+                break
+            case 'week':
+                data.push(
+                    {
+                        iterable : ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'],
+                        property : (m) => {
+                            return new Date(m.timestamp_ms).format('DD')
+                        }
+                    }
+                )
+                break
+            case 'weekhour':
+                data.push(
+                    {
+                        iterable : ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'],
+                        property : (m) => {
+                            return new Date(m.timestamp_ms).format('DD')
+                        }
+                    },
+                    {
+                        iterable : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+                        property : (m) => {
+                            return new Date(m.timestamp_ms).format('h')
+                        }
+                    }
+                )
+                break
+            case 'day':
+                data.push(
+                    {
+                        iterable : this.iterableDate('yyyy-MM-dd','addDays'),
+                        property : (m) => {
+                            return new Date(m.timestamp_ms).format('yyyy-MM-dd')
+                        }
+                    }
+                )
+                break
+            case 'month':
+                data.push(
+                    {
+                        iterable : this.iterableDate('yyyy-MM','addMonths'),
+                        property : (m) => {
+                            return new Date(m.timestamp_ms).format('yyyy-MM')
+                        }
+                    }
+                )
+                break
+            case 'year':
+                data.push(
+                    {
+                        iterable : this.iterableDate('yyyy','addYears'),
+                        property : (m) => {
+                            return new Date(m.timestamp_ms).format('yyyy')
+                        }
+                    }
+                )
+                break
+            default:
+                return messagesList.length
+        }
+        if (participantList){
+            data.push(
+                {
+                    iterable : participantList,
+                    property : (m) => {
+                        return m.sender_name
+                    }
+                }
+            )
+        }
+
+        this.createRet(ret, data)
+        for (fct of process) {
+            fct(ret, data)
+        }
+        return ret
+    },
+    /**
+     * create an array whit all formatted date as string
+     * @param dateFormat
+     * @param addMethod
+     */
+    iterableDate : function(dateFormat, addMethod) {
+        let ret = []
+        for (let i = new Date(startDate) ; i.format(dateFormat)<=endDate.format(dateFormat) ; i[addMethod](1)){
+            ret.push(i.format(dateFormat))
+        }
+        return ret
+    },
+    /**
+     * create the object that contain the data
+     * @param parent
+     * @param data =>array(
+     *  {
+     *      iterable : array of key,
+     *  },
+     *  [...]
+     *)
+     */
+    createRet : function(parent, data) {
+        for (key of data[0].iterable){
+            parent[key] = data[1] ? {} : 0
+            if (data[1])
+                this.createRet(parent[key], data.slice(1))
+        }
+    },
+    /**
+     * fill the object that contain the data
+     * @param message
+     * @param parent
+     * @param data =>array(
+     *  {
+     *      iterable : array of key,
+     *      value : the value of the key (obj or number)
+     *  },
+     *  [...]
+     *)
+     * @param process
+     */
+    fillRet : function(message, parent, data, process) {
+        if (data[1]){
+            this.fillRet(message, parent[data[0].property(message)], data.slice(1), process)
+        }else{
+            process(message, parent, data)
+        }
     }
 }
 //</editor-fold>
